491effad350d13315335ad381eb30137a1a69069
brunoN80 <bruno_rocha2008@hotmail.com>
2020-06-04 13:55:01 -0300

    ajuste de login e header

--- components/Header/index.js
+++ components/Header/index.js
@@ -30,19 +30,19 @@ function Header({ isAuthenticated }) {
     }
 
     return (
-        <div className="p-2 flex flex-col justify-between items-center xl:flex-row lg:flex-row md:flex-row sm:flex-col">
+        <div className="p-2 flex flex-col justify-between items-center xl:flex-row lg:flex-row md:flex-row sm:flex-col w-full px-5">
             <Link href="/">
                 <div className="flex align-middle items-center cursor-pointer">
-                    <img className="ml-4" width={50} src="https://i.pinimg.com/originals/b1/5e/ed/b15eedbdafbbdbca3249e3942f4faf3b.png" alt="github logo" />
+                    <img className="" width={50} src="https://i.pinimg.com/originals/b1/5e/ed/b15eedbdafbbdbca3249e3942f4faf3b.png" alt="github logo" />
                     <h1 className="text-xl ml-4">Github Profile Search</h1>
                 </div>
             </Link>
             {
                 isAuthenticated && (
-                    <div className="flex flex-col mr-4">
-                        <div className="flex items-center">
+                    <div className="flex flex-col">
+                        <div className="flex items-center justify-between">
                             <Link href="/profile" className="text-xl mr-4"><a className="text-xl">{user.name}</a></Link>
-                            <img className="rounded-full ml-4" width={40} src={user.avatar_url} alt="" />
+                            <img className="rounded-full ml-5" width={40} src={user.avatar_url} alt="" />
                         </div>
                         <button className="self-start rounded px-5 bg-gray-700 text-white" type="button" onClick={logout}>Sair</button>
                     </div>
